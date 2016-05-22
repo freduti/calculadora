25e0337869eb2072f9bfbb247d1a173fdca2d56a
Freed <buscando_alink@hotmail.com>
2016-05-22 17:53:48 -0400

    Avances
    La calculadora ya suma!
    Y  agregamos un archivo en js para ver la diferencia con python

---
+++ index.js
@@ -0,0 +1,7 @@
+// 		js		 python
+// console.log = print
+if (1 > 10) {
+console.log('uno es mayor que diez????');
+} else {
+console.log('uno es menor que diez!');
+}
--- index.py
+++ index.py
@@ -1,12 +1,47 @@
-# aqui inicia nuestra calculadora
-print 'Bienvenido a nuestra calculadora OP'
+# identacion
 
-# le damos opciones al usuario
-print '1) sumar'
-print '2) restar'
-print '3) multiplicar'
-print '4) dividir'
-print "5) salir"
+if 1 != 2 :
+	print 'uno no es igual a dos :l'
 
-# leemos lo que el usuario ha escrito :)
-opcion = raw_input()
+
+# # int = integer
+# # str = string
+
+# # aqui inicia nuestra calculadora
+# print 'Bienvenido a nuestra calculadora OP'
+
+# # le damos opciones al usuario
+# print '1) sumar'
+# print '2) restar'
+# print '3) multiplicar'
+# print '4) dividir'
+# print "5) salir"
+
+# # leemos lo que el usuario ha escrito :)
+# # nota: opcion aqui es una cadena de texto
+# opcion = raw_input('opcion >> ')
+
+# # y aqui es un numero entero :D
+# opcion = int(opcion)
+
+# if opcion > 5 or opcion < 1 :
+# 	print 'esa opcion no es valida :l'
+# else:
+# 	# aqui opcion ya no es un string
+# 	if opcion == 1:
+# 		# sumar +
+# 		print 'vamos a sumar!!\n'
+# 		numero1 = int(raw_input('primer numero >>'))
+# 		numero2 = int(raw_input('segundo numero >>'))
+# 		print numero1 - numero2
+# 	if opcion == 2:
+# 		# restar -
+# 		print "Vamos a Restar!!"
+# 	if opcion == 3:
+# 		# multiplicar *
+# 		print "vamos a Multiplicar!!"
+# 	if opcion == 4:
+# 		#dividir /
+# 		print "Vamos a dividir!!"
+# 	if opcion == 5:
+# 		print "Adios!"
