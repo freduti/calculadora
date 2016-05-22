edc8adcbdbfc9cae692ff2357456a28fbc7c33a0
Freed <buscando_alink@hotmail.com>
2016-05-22 16:49:48 -0400

    Preparando calculadora

---
+++ ejecutar.bat
@@ -0,0 +1,2 @@
+cls
+python index.py
---
+++ index.py
@@ -0,0 +1,12 @@
+# aqui inicia nuestra calculadora
+print 'Bienvenido a nuestra calculadora OP'
+
+# le damos opciones al usuario
+print '1) sumar'
+print '2) restar'
+print '3) multiplicar'
+print '4) dividir'
+print "5) salir"
+
+# leemos lo que el usuario ha escrito :)
+opcion = raw_input()
